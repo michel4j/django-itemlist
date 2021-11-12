c18972caaf1acadda276bc4ac37046382528a802
Michel Fodje <michel4j@gmail.com>
2021-11-12 09:35:34 -0600

    Add requirements file

--- setup.py
+++ setup.py
@@ -7,6 +7,8 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
+with open(os.path.join(os.path.dirname(__file__), 'requirements.txt')) as f:
+    requirements = f.read().splitlines()
 
 setup(
     name='django-itemlist',
@@ -20,10 +22,11 @@ setup(
     description='A customizable Django Admin ChangeList-like app for use outside of the admin.',
     long_description=README,
     long_description_content_type='text/x-rst',
+    install_requires=requirements,
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
-        'Framework :: Django :: 3.1',
+        'Framework :: Django :: 3',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
